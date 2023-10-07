5af839fa4cbb7a339118370c4755b4266ca47ccf
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-10-07 11:29:12 +0300

    Everything extra

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  return fetch('https://anapioficeandfire.com/api/books').then(res => res.json()).then(json => renderBooks(json));
+  fetch('https://anapioficeandfire.com/api/books').then(res => res.json()).then(json => renderBooks(json));
   // fetch('https://anapioficeandfire.com/api/books').then(res => res.json()).then(data => console.log(renderBooks()));
-  // fetch('https://anapioficeandfire.com/api/books').then(res => res.json()).then(data => console.log(renderBooks));
+  return fetch('https://anapioficeandfire.com/api/books').then(res => res.json()).then(json => render5th(json));
   // console.log(renderBooks(data));
   // console.log('Inside fetchBooks()');
   // console.log(renderBooks);
+}
+
+
+// This does all the extra work in the assignment page: 
+// LOGs books in the specific indexx position
+// after it gets the value of the character stored in that position which happens to be another api of itself we then use fetch to access it and collect the wanted data
+
+function render5th(books) {
+  console.log(books);
+  console.log(books[4]);
+  console.log(books[4].characters[1030])
+  const linkChar = books[4].characters[1030]
+  fetch(linkChar).then(res => res.json()).then(json => console.log(`This is the 1031 character: ${json.name}`));
+
+  books.forEach(book => {
+    console.log(`The number of pages of each book is: ${book.numberOfPages} of the book: ${book.name}`)
+  });
+
 }
 
 function renderBooks(books) {
